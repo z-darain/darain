56453980e32c1bfd9ef0f49c52703377e7a958fe
z-darain <darainzahra854@gmail.com>
2024-11-13 10:18:58 +0500

    Add files via upload

---
+++ lab 8 1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int rows, col;
+
+
+cout<<"enter rows:";
+cin>>rows;
+cout<<"enter coulumns:";
+cin>>col;
+int arry[rows][col];
+
+//input elemnts
+int elements;
+cout<<"enter the elements";
+
+for(int i=0; i<rows; i++)
+{
+	for(int j=0; j<col; j++)
+	{
+		cout<<"\nelements["<<i<<"]["<<j<<"]:";
+		cin>>arry[i][j];	}
+	
+}
+cout<<"\n the 2D array is:\n";
+for(int i=0; i <rows; i++)
+{
+	for(int j=0; j<col; j++)
+	{
+		cout<<arry[i][j]<< " " ;
+			int sum= sum + arry [i] [j];
+cout<<"the sum is\n:"<<sum<<endl;
+
+	int average=sum/rows+col;
+cout<<"average is:"<<average<<endl;
+		
+	}
+
+}
+
+	
+		
+
+ return 0;
+}
